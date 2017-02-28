ae2b0c1502be1f728fe8fab8354c500866aff0c7
Tim Evearitt <timevearitt@gmail.com>
2017-02-27 20:07:17 -0700

    WIP Update

--- Pomodoro/scripts/scripts.js
+++ Pomodoro/scripts/scripts.js
@@ -1,36 +1,47 @@
 $(document).ready(function() {
-	var workTime = 5;
-	var breakTime = 300;
-	var isTiming = true;
-	var workInterval = setInterval(function(){ setWorkTime() }, 1000);
-
-	$("#workTimer").html(Math.floor(workTime/60) + ":" + workTime % 60);
-	$("#breakTimer").html(Math.floor(breakTime/60) + ":" + breakTime % 60);
-
-
-		function setWorkTime(){
-			workTime--;
-			$("#workTimer").html(Math.floor(workTime/60) + ":" + workTime % 60);
-			if(workTime === 0){
-				clearInterval(workInterval);
-				breakInterval = setInterval(function(){ setBreakTime() }, 1000);
-			}
-		}
-
-		function setBreakTime(){
-			breakTime--;
-			$("#breakTimer").html(Math.floor(breakTime/60) + ":" + breakTime % 60);
-			if(breakTime === 0){
-				clearInterval(breakInterval);
-				breakInterval = setInterval(function(){ setBreakTime() }, 1000);
-			}
-		}
-			
-
-
-
-
+	var startWorkTime = 5;
+	var workTime = startWorkTime;
+	var startBreakTime = 5;
+	var breakTime = startBreakTime;
+	var isTiming = false;
+	var workInterval;
 	
-		
 
+	$("#workTimer").html(Math.floor(workTime/60) + ":" + returnSeconds(workTime));
+	$("#breakTimer").html(Math.floor(breakTime/60) + ":" + returnSeconds(breakTime));
+
+	$("#start").click(function() {
+		isTiming = true;
+		workInterval = setInterval(function(){ setWorkTime() }, 1000);
+	});
+
+
+	function setWorkTime(){
+		workTime--;
+		$("#workTimer").html(Math.floor(workTime/60) + ":" + returnSeconds(workTime));
+		if(workTime === 0){
+			clearInterval(workInterval);
+			breakTime = 5;
+			breakInterval = setInterval(function(){ setBreakTime() }, 1000);
+		}
+	}
+	
+	function setBreakTime(){
+		breakTime--;
+		$("#breakTimer").html(Math.floor(breakTime/60) + ":" + returnSeconds(breakTime));
+		if(breakTime === 0){
+			clearInterval(breakInterval);
+			workTime = 5;
+			workInterval = setInterval(function(){ setWorkTime() }, 1000);
+		}
+	}
+	
+	function returnSeconds(num){
+		if(num % 60 < 10){
+			return "0" + num%60
+		}else{
+			return num % 60;
+		}
+	}
+			
 });
