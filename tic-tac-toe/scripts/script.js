6f855e3078d3e9eb89edd0b2aae46db720bd306a
Tim Evearitt <timevearitt@gmail.com>
2017-03-19 15:21:50 -0600

    tic tac toe updates

--- tic-tac-toe/scripts/script.js
+++ tic-tac-toe/scripts/script.js
@@ -1,28 +1,125 @@
 $(document).ready(function() {
-	var playerXO = "X";
+	var board = [
+				["","",""],
+				["","",""],
+				["","",""]
+			];
+	var isPlayerTurn = true;
+	var isGameOver = false;
+	var turnCount = 0;
+	var gameNum = 0;
+	var player = {token:"X", wins: 0};
+	var ai = {token:"O", wins: 0};
 
-	$("#X").click(function() {
-		reset();
-		$("#xoro").hide();
-	});
-
-	$("#O").click(function() {
-		playerXO = "O";
-		reset();
-		$("#xoro").hide();
-	});
+	console.log(player.token);
 
 	$(".square").click(function(event){
-		sid = "#" + event.target.id;
-		text = $(sid).text();
-		if(text === null || text===""){
-			$(sid).html(playerXO);
-		}
+			sid = event.target.id;
+			row = sid.substring(0,1);
+			col = sid.substring(1,2);
+			if(board[row][col] === ""){
+				board[row][col] = player.token;
+				isPlayerTurn = false;
+				turnCount++;
+				updateBoard();
+				console.log(board);
+			}
 	});
+	
+		
 
-	function reset(){
-		$(".square").html("");
+			//if player or ai turn
+			if(isPlayerTurn === true){
+				//playerTurn();
+				
+			}else{
+				aiTurn();
+				updateBoard();
+			}
+			//update board
+			
+			//is game over??
+			//isGameOver = gameOver();
+		
+	
+
+	
+		
+	
+
+	function aiTurn(){
+		row = Math.floor(Math.random() * 3);
+		col = Math.floor(Math.random() * 3);
+
+		if(board[row][col] !== ""){
+			board[row][col] = ai.token;
+			isPlayerTurn = true;
+			turnCount++;
+		}
 	}
 
+	function initGame(){
+		board = [
+				["","",""],
+				["","",""],
+				["","",""]
+			];
+
+		turnCount = 0;
+		gameNum++;
+	}
+
+	function updateBoard(){
+		for(i=0; i<3; i++){
+			for(j=0; j<3; j++){
+				$("#" + i + j).html(board[i][j]);
+			}
+		}
+	}
+
+	function gameOver(){
+				//check rows for win
+		for(i=0; i<3; i++){
+			if(board[i][0] === board[i][1] && board[i][0] === board[i][2] && board[i][0] != ""){
+				console.log(board[i][0] + " WINS!");
+				alert(board[i][0] + " WINS!");
+				incrementScore(board[i][0]);
+				return true;
+			}
+		}
+		//check column for win
+		for(j=0; j<3; j++){
+			if(board[0][j] === board[1][j] && board[0][j] === board[2][j] && board[0][j] != ""){
+				console.log(board[0][j] + " WINS!");
+				alert(board[0][j] + " WINS!");
+				incrementScore(board[0][j]);
+				return true;
+			}
+		}
+
+		//check top left to bottom right
+		if(board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] != ""){
+			console.log(board[0][0] + " WINS!");
+			alert(board[0][0] + " WINS!");
+			incrementScore(board[0][0]);
+			return true;
+		}
+
+		//check top right to bottom left
+		if(board[2][0] === board[1][1] && board[2][0] === board[0][2] && board[2][0] != ""){
+			console.log(board[2][0] + " WINS!");
+			alert(board[2][0] + " WINS!");
+			incrementScore(board[2][0]);
+			return true;
+		}
+
+		//check for draw
+		if(turnCount === 9){
+			return true;
+		}
+
+		return false;
+
+	}
 
 });
